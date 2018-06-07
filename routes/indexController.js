62c6955c42ccd982ab15442b39318cec684d63da
Scottl4784 <Scottl4784@gmail.com>
2018-06-07 16:28:09 -0400

    Began working on series router

--- routes/indexController.js
+++ routes/indexController.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 router.get('/', (req, res) => {
     res.render('index')
 })
+
 
 
 module.exports = router
--- routes/seriesController.js
+++ routes/seriesController.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const Series = require('../models/series.js')
+const Series = require('../models/series')
 
+router.get('/', (req, res, next) => {
+    Series
+    .find()
+    .then((seriesList) => {
+        res.render('series/index', {seriesList})
+    })
+    .catch((err) => res.send(err))
+})
 
 module.exports = router

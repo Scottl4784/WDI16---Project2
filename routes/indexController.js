989c5a3b42c6168fc39e4e985afc2b51c02209de
Scottl4784 <Scottl4784@gmail.com>
2018-06-08 10:33:05 -0400

    Began adding routes for seriesController

--- routes/indexController.js
+++ routes/indexController.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
+const UserModel = require('../models/user')
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.post('/login', (req, res) => {
+    const userName = req.body.username
+    UserModel.findOne({ 'username': userName })
+        .then((user) => {
+            res.redirect(`/${user.username}`)
+        })
+        .catch((err) => {
+            res.render("error", {
+                message: "User doesn't exist",
+                err
+            })
+        })
+
+})
 
 
 
--- routes/seriesController.js
+++ routes/seriesController.js
@@ -2,13 +2,47 @@ const express = require('express')
 const router = express.Router()
 const Series = require('../models/series')
 
+// Find all series
 router.get('/', (req, res, next) => {
     Series
-    .find()
-    .then((seriesList) => {
-        res.render('series/index', {seriesList})
-    })
-    .catch((err) => res.send(err))
+        .find()
+        .then((seriesList) => {
+            res.render('series/index', { seriesList })
+        })
+        .catch((err) => res.send(err))
+})
+
+// New
+router.get('/new', (req, res) => {
+    res.render('series/new')
+})
+
+// Create
+router.post('/', (req, res) => {
+    const newSeries = req.body
+    Series
+        .create(newSeries)
+        .then(() => {
+            res.redirect('/series')
+        })
+})
+
+// Show
+router.get('/:id', (req, res) => {
+    Series
+        .findById(req.params.id)
+        .then((seriesList) => {
+            res.render('series/show', seriesList)
+        })
+})
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+    Series
+        .findById(req.params.id)
+        .then((series) => {
+            res.render('series/edit', { seriesList })
+        })
 })
 
 module.exports = router
